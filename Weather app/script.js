b7d719dbe069c7e066d59ecc77228d7d8d7866f4
Vinisha <127471045+vinisha-04@users.noreply.github.com>
2023-12-13 23:37:14 +0530

    Add files via upload

---
+++ Weather app/script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '93a32a303cmsh644763be77111f7p136203jsnac4e0e2b880b',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
